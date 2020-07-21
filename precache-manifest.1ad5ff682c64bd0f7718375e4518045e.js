9aad11ec2ae47aeb7f79f9c695da41b9785d265e
Nikola Stojanovic <nikola.stojanovic@computerrock.com>
2020-07-21 15:55:40 +0200

    react

---
+++ precache-manifest.1ad5ff682c64bd0f7718375e4518045e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55d74b7699d65984f185bf3b18b9a31d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d2a2735bf8f8dc9f6d2",
+    "url": "/static/css/3.f209e947.chunk.css"
+  },
+  {
+    "revision": "cc772c13a3ddf1ad0a57",
+    "url": "/static/css/main.c011749b.chunk.css"
+  },
+  {
+    "revision": "4345b680ba5bc399a0a4",
+    "url": "/static/js/2.ae18a78a.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.ae18a78a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d2a2735bf8f8dc9f6d2",
+    "url": "/static/js/3.85583d20.chunk.js"
+  },
+  {
+    "revision": "cc772c13a3ddf1ad0a57",
+    "url": "/static/js/main.1acb2884.chunk.js"
+  },
+  {
+    "revision": "0c6987925e0dab544837",
+    "url": "/static/js/runtime-main.bd1d5460.js"
+  }
+]);

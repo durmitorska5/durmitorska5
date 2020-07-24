f9dea9ad1a13abeb0b10a9c20b0f52a25bd2c366
Nikola Stojanovic <nikola.stojanovic@computerrock.com>
2020-07-24 10:18:50 +0200

    link styling changes, added serbian latin chars

--- src/containers/Dashboard/Dashboard.js
+++ src/containers/Dashboard/Dashboard.js
@@ -21,7 +21,7 @@ class Dashboard extends Component {
             <h2>VESTI</h2>
             <p>Datum: 24-07-2020</p>
             <p>
-              Sajt je azuriran novim podacima u vezi sa dosadasnjim uplatama na racun SZ i nabavkama koje su sprovedene. Nedostaju pojedine fakture i dokumentacija zbog situacije sa pandemijom. Zaposleni u firmi Kosmaj rade od kuće pa nemaju pristup celoj arhivi, ali u planu je da svu dokumenataciju postavimo na sajt.
+              Sajt je ažuriran novim podacima u vezi sa dosadašnjim uplatama na račun SZ i nabavkama koje su sprovedene. Nedostaju pojedine fakture i dokumentacija zbog situacije sa pandemijom. Zaposleni u firmi Kosmaj rade od kuće pa nemaju pristup celoj arhivi, ali u planu je da svu dokumenataciju postavimo na sajt.
             </p>
             <p>
               Nadamo se da ćete učestvovati u radu, sugestije možete dostaviti
@@ -37,58 +37,58 @@ class Dashboard extends Component {
             </p>
           </article>
           <article>
-            <h2>OPSTE</h2>
-            <a href="/docs/Isplate.pdf" target="_blank">Isplate.pdf</a>
-            <a href="/docs/Obavestenje.pdf" target="_blank">Obavestenje.pdf</a>
-            <a href="/docs/Uplate i dugovanja.pdf" target="_blank">Uplate i dugovanja.pdf</a>
+            <h2>OPŠTE</h2>
+            <a href="/docs/Isplate.pdf" target="_blank" className="Button Success">Isplate.pdf</a>
+            <a href="/docs/Obavestenje.pdf" target="_blank" className="Button Success">Obavestenje.pdf</a>
+            <a href="/docs/Uplate i dugovanja.pdf" target="_blank" className="Button Success">Uplate i dugovanja.pdf</a>
           </article>
           <article>
-            <h2>RACUNI ZA KATANCE</h2>
-            <a href="/docs/Ostalo/Racuni - katanci.pdf" target="_blank">Racuni - katanci.pdf</a>
+            <h2>RAČUNI ZA KATANCE</h2>
+            <a href="/docs/Ostalo/Racuni - katanci.pdf" target="_blank" className="Button Success">Racuni - katanci.pdf</a>
           </article>
           <article>
             <h2>PRAVILNIK</h2>
-            <a href="/docs/Pravilnik/Kucni red.pdf" target="_blank">Kucni red.pdf</a>
+            <a href="/docs/Pravilnik/Kucni red.pdf" target="_blank" className="Button Success">Kucni red.pdf</a>
           </article>
           <article>
-            <h2>RACUN SZ</h2>
-            <a href="/docs/Racun SZ/09_04_2019_Uplate i dugovanja.pdf" target="_blank">09_04_2019_Uplate i dugovanja.pdf</a>
-            <a href="/docs / Racun SZ /09_05_2019_Uplate i dugovanja.pdf" target="_blank">09_05_2019_Uplate i dugovanja.pdf</a>
-            <a href="/docs / Racun SZ /23_07_2020_Isplate.pdf" target="_blank">23_07_2020_Isplate.pdf</a>
-            <a href="/docs / Racun SZ /23_07_2020_Uplate i dugovanja.pdf" target="_blank">23_07_2020_Uplate i dugovanja.pdf</a>
-            <a href="/docs / Racun SZ / Izvodi / izvodi.zip" target="_blank"> izvodi.zip</a>
+            <h2>RAČUN SZ</h2>
+            <a href="/docs/Racun SZ/09_04_2019_Uplate i dugovanja.pdf" target="_blank" className="Button Success">09_04_2019_Uplate i dugovanja.pdf</a>
+            <a href="/docs / Racun SZ /09_05_2019_Uplate i dugovanja.pdf" target="_blank" className="Button Success">09_05_2019_Uplate i dugovanja.pdf</a>
+            <a href="/docs / Racun SZ /23_07_2020_Isplate.pdf" target="_blank" className="Button Success">23_07_2020_Isplate.pdf</a>
+            <a href="/docs / Racun SZ /23_07_2020_Uplate i dugovanja.pdf" target="_blank" className="Button Success">23_07_2020_Uplate i dugovanja.pdf</a>
+            <a href="/docs / Racun SZ / Izvodi / izvodi.zip" target="_blank" className="Button Success"> izvodi.zip</a>
           </article>
           <article>
           <h2>REMONT LIFTA</h2>
-            <a href="/docs/Remont lifta/Naplata - Remont lifta - 04-06-2020.pdf" target="_blank">Naplata - Remont lifta - 04-06-2020.pdf</a>
-            <a href="/docs / Remont lifta / Ponuda - Remont lifta.pdf" target="_blank"> Ponuda - Remont lifta.pdf</a>
-            <a href="/docs / Remont lifta / Potpisi - Remont lifta.pdf" target="_blank"> Potpisi - Remont lifta.pdf</a>
-            <a href="/docs / Remont lifta / Ugovor - Remont lifta.pdf" target="_blank"> Ugovor - Remont lifta.pdf</a>
+            <a href="/docs/Remont lifta/Naplata - Remont lifta - 04-06-2020.pdf" target="_blank" className="Button Success">Naplata - Remont lifta - 04-06-2020.pdf</a>
+            <a href="/docs / Remont lifta / Ponuda - Remont lifta.pdf" target="_blank" className="Button Success"> Ponuda - Remont lifta.pdf</a>
+            <a href="/docs / Remont lifta / Potpisi - Remont lifta.pdf" target="_blank" className="Button Success"> Potpisi - Remont lifta.pdf</a>
+            <a href="/docs / Remont lifta / Ugovor - Remont lifta.pdf" target="_blank" className="Button Success"> Ugovor - Remont lifta.pdf</a>
           </article>
           <article>
             <h2>UPRAVLJANJE ZGRADOM</h2>
-            <a href="/docs/Upravljanje zgradom/Odluka - Tekuce i investiciono odrzavanje.pdf" target="_blank">Odluka - Tekuce i investiciono odrzavanje.pdf</a>
-            <a href="/docs / Upravljanje zgradom / Ponuda - Upravljanje.pdf" target="_blank"> Ponuda - Upravljanje.pdf</a>
-            <a href="/docs / Upravljanje zgradom / Ugovor - Infostan.pdf" target="_blank"> Ugovor - Infostan.pdf</a>
-            <a href="/docs / Upravljanje zgradom / Ugovor - Upravljanje SZ.pdf" target="_blank"> Ugovor - Upravljanje SZ.pdf</a>
+            <a href="/docs/Upravljanje zgradom/Odluka - Tekuce i investiciono odrzavanje.pdf" target="_blank" className="Button Success">Odluka - Tekuce i investiciono odrzavanje.pdf</a>
+            <a href="/docs / Upravljanje zgradom / Ponuda - Upravljanje.pdf" target="_blank" className="Button Success"> Ponuda - Upravljanje.pdf</a>
+            <a href="/docs / Upravljanje zgradom / Ugovor - Infostan.pdf" target="_blank" className="Button Success"> Ugovor - Infostan.pdf</a>
+            <a href="/docs / Upravljanje zgradom / Ugovor - Upravljanje SZ.pdf" target="_blank" className="Button Success"> Ugovor - Upravljanje SZ.pdf</a>
           </article>
           <article>
-            <h2>VESERNICA</h2>
-            <a href="/docs/Vesernica/Ugovor - Vesernica.pdf" target="_blank">Ugovor - Vesernica.pdf</a>
+            <h2>VEŠERNICA</h2>
+            <a href="/docs/Vesernica/Ugovor - Vesernica.pdf" target="_blank" className="Button Success">Ugovor - Vesernica.pdf</a>
           </article>
           <article>
             <h2>VIDEO NADZOR</h2>
-            <a href="/docs/Video nadzor/1-Zakon o privatnom obezbedjenju.pdf" target="_blank">1-Zakon o privatnom obezbedjenju.pdf</a>
-            <a href="/docs / Video nadzor / 2 - Zakon o izmenama i dopunama  zakona o privatnom obezbedjenju 87_2018.pdf" target="_blank"> 2 - Zakon o izmenama i dopunama  zakona o privatnom obezbedjenju 87_2018.pdf</a>
-            <a href="/docs / Video nadzor / Izvod_video_nadzor.pdf" target="_blank"> Izvod_video_nadzor.pdf</a>
-            <a href="/docs / Video nadzor / Ponuda video nadzor.pdf" target="_blank"> Ponuda video nadzor.pdf</a>
-            <a href="/docs / Video nadzor / Zapisnici i potvrde_video nadzor.pdf" target="_blank"> Zapisnici i potvrde_video nadzor.pdf</a>
+            <a href="/docs/Video nadzor/1-Zakon o privatnom obezbedjenju.pdf" target="_blank" className="Button Success">1-Zakon o privatnom obezbedjenju.pdf</a>
+            <a href="/docs / Video nadzor / 2 - Zakon o izmenama i dopunama  zakona o privatnom obezbedjenju 87_2018.pdf" target="_blank" className="Button Success"> 2 - Zakon o izmenama i dopunama  zakona o privatnom obezbedjenju 87_2018.pdf</a>
+            <a href="/docs / Video nadzor / Izvod_video_nadzor.pdf" target="_blank" className="Button Success"> Izvod_video_nadzor.pdf</a>
+            <a href="/docs / Video nadzor / Ponuda video nadzor.pdf" target="_blank" className="Button Success"> Ponuda video nadzor.pdf</a>
+            <a href="/docs / Video nadzor / Zapisnici i potvrde_video nadzor.pdf" target="_blank" className="Button Success"> Zapisnici i potvrde_video nadzor.pdf</a>
           </article>
           <article>
             <h2>VRATA I PRISTUP</h2>
-            <a href="/docs/Vrata i pristup/Citac_kartica_izvod.pdf" target="_blank">Citac_kartica_izvod.pdf</a>
-            <a href="/docs / Vrata i pristup / Popravka_vrata_oprema_izvod.pdf" target="_blank"> Popravka_vrata_oprema_izvod.pdf</a>
-            <a href="/docs / Vrata i pristup / Popravka_vrata_racun.pdf" target="_blank"> Popravka_vrata_racun.pdf</a>
+            <a href="/docs/Vrata i pristup/Citac_kartica_izvod.pdf" target="_blank" className="Button Success">Citac_kartica_izvod.pdf</a>
+            <a href="/docs / Vrata i pristup / Popravka_vrata_oprema_izvod.pdf" target="_blank" className="Button Success"> Popravka_vrata_oprema_izvod.pdf</a>
+            <a href="/docs / Vrata i pristup / Popravka_vrata_racun.pdf" target="_blank" className="Button Success"> Popravka_vrata_racun.pdf</a>
           </article>
           <article>
             <h2>ZAKON</h2>
